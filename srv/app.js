26a023e9b37c6cdf0bd6805a272ab64b2c928c22
Mohamed Ahmednah <mohamed@teleportd.com>
2012-03-30 15:13:18 +0200

    fb login added

--- srv/app.js
+++ srv/app.js
@@ -3,7 +3,7 @@
 var express = require("express");
 var app = express.createServer();
 var io = require('socket.io').listen(app);
-
+var http = require('http');
 var config = require('./../shared/config.js').config;
 
 // Configuration
@@ -75,6 +75,33 @@ app.get('/score/:id?', function(req, res, next) {
   }
 });
 
+//endpoint used by fb plugin client side
+app.get('/chn', function(req, res, next){
+  res.send('<script src="//connect.facebook.net/en_US/all.js"></script>');
+  
+});
+
+
+app.get('/login',function(req, res, next){
+  var accessToken = req.params.accessToken;
+  var options = {
+    host: 'https://graph.facebook.com',
+    port: 80,
+    path: '/?accessToken=' + accessToken,
+    method: 'GET'
+  };
+  console.log("hello");
+  res.send("hello");
+  var req = http.request(options, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      console.log('BODY: ' + chunk);
+    });
+  });
+  
+});
 app.listen(8080);
 
 console.log('socket.io server started on port 8080');
